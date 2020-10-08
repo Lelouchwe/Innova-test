46ffd97c9c62a7cfd6d2cfd6bfacab3c135505d6
Vitaliy <vitalikshubnikov@mail.ru>
2020-10-08 13:40:27 +0300

    init space

---
+++ src/views/Post.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+        <h1>hello</h1>
+        <comments/>
+  </div>
+</template>
+
+<script>
+export default {
+    components: {
+        comments: () => import('@/components/comments.vue')
+    }
+}
+</script>
+
+<style>
+
+</style>
