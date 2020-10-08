f8663a202a14c7967c8d8018b69b71b28952dc8d
Vitaliy <vitalikshubnikov@mail.ru>
2020-10-08 16:37:51 +0300

    recursive three comments

--- src/components/comments.vue
+++ src/components/comments.vue
@@ -1,16 +1,85 @@
 <template>
   <div class="container">
-      <h1>comment 1</h1>
-      <h1>comment 2</h1>
+        <commentsItem 
+            v-for="(item, index) in comments" :key="index"
+            :item="item"
+        />
+        <div class="show-more">
+            Показать еще
+        </div>
   </div>
 </template>
 
 <script>
+import commentsJson from "@/comments.json"
 export default {
+  components: {
+      commentsItem: () => import('@/components/commentsItem.vue')
+  },
+  data() {
+    return {
+      comments: [
+        {
+          id: 1,
+          name: 'john',
+          content: 'something text here',
+          child: [
+            {
+              id: 12,
+              name: 'john',
+              content: 'something text here',
+              child: [
+                {
+                id: 12,
+                name: 'john',
+                content: 'something text here'
+                },
+                {
+                id: 13,
+                name: 'john',
+                content: 'something text here'
+                },
+            ]
+            },
+            {
+              id: 13,
+              name: 'john',
+              content: 'something text here'
+            },
+          ]
+        },
+        {
+          id: 2,
+          name: 'wick',
+          content: 'something text here'
+        },
+        {
+          id: 3,
+          name: 'ger',
+          content: 'something text here'
+        },
+      ],
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+      console.log(commentsJson);
+  }
 
 }
 </script>
 
 <style>
-
+.sub-comments {
+    margin: 0 20px;
+}
+.show-more {
+    margin-top: 20px;
+    cursor: pointer;
+}
+.show-more:hover {
+    opacity: .8;
+}
 </style>
---
+++ src/components/commentsItem.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <div class="comment-item">
+            <h1>{{item.name}}</h1>
+            <p>{{item.content}}</p>
+        </div>
+        <div class="child-comment" v-if="item.child">
+            <commentsItem 
+                v-for="(sub, index) in item.child" :key="index" 
+                class="sub-comments"
+                :item="sub"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'commentsItem',
+    props: ['item'],
+    data() {
+        return {
+        }
+    },
+    methods: {
+
+    }
+
+}
+</script>
+
+<style>
+.sub-comments {
+  margin: 0 20px;
+}
+.show-more {
+  cursor: pointer;
+}
+.show-more:hover {
+  opacity: .8;
+}
+.comment-item {
+    padding: 10px;
+    transition: .3s;
+    border-radius: 10px;
+}
+.comment-item:hover {
+    background: #8080800c;
+}
+.child-comment {
+    border-left: 1px solid #80808063;
+}
+</style>
